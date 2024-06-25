a31c589d68a25bca669739c29e9ea78f724cd9b9
Naoto Abe <naotoabe1@gmail.com>
2024-06-25 16:51:08 +0200

    Initial Commit

---
+++ akaPrep/akaPrepApp.swift
@@ -0,0 +1,20 @@
+//
+//  akaPrepApp.swift
+//  akaPrep
+//
+//  Created by Naoto Abe on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct akaPrepApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
